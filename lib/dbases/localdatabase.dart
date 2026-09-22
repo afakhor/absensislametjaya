1f7d0bb7a19606e963a1bc237c9b7bc1d1fe9193
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-22 12:00:59 +0700

    Update localdatabase.dart

--- lib/dbases/localdatabase.dart
+++ lib/dbases/localdatabase.dart
@@ -46,9 +46,34 @@ class AuditLog extends Table {
   TextColumn get detail => text()();
   TextColumn get status => text()();
 }
+// TAMBAHAN BARU - LOG SIMULASI LABA
+class SimulasiLaba extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  DateTimeColumn get tanggalSimulasi => dateTime().withDefault(currentDateAndTime)();
+  DateTimeColumn get periodeMulai => dateTime()();
+  DateTimeColumn get periodeSelesai => dateTime()();
+  IntColumn get omset => integer()();
+  IntColumn get pemasukanLain => integer()();
+  IntColumn get bebanOperasional => integer()();
+  IntColumn get bebanLain => integer()();
+  IntColumn get bebanGaji => integer()();
+  IntColumn get totalPemasukan => integer()();
+  IntColumn get totalBeban => integer()();
+  IntColumn get labaKotor => integer()();
+  IntColumn get labaBersih => integer()();
+  TextColumn get catatan => text().nullable()();
+}
 
-@DriftDatabase(tables: [KategoriKaryawan, Karyawan, Absensi, GajiMingguan, Transaksi, AuditLog])
+@DriftDatabase(tables: [KategoriKaryawan, Karyawan, Absensi, GajiMingguan, Transaksi, AuditLog, SimulasiLaba])
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(driftDatabase(name: 'tb_slamet_jaya_v5_owner_foto'));
-  @override int get schemaVersion => 1;
+  @override int get schemaVersion => 2;
+  @override MigrationStrategy get migration => MigrationStrategy(
+    onCreate: (m) async => await m.createAll(),
+    onUpgrade: (m, from, to) async {
+      if (from < 2) {
+        await m.createTable(simulasiLaba);
+      }
+    }
+  );
 }
