9673de3b5d7ef93a954ce5534fb607f536cba19e
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-22 12:01:25 +0700

    Update laba_db.dart

--- lib/dbases/laba_db.dart
+++ lib/dbases/laba_db.dart
@@ -2,6 +2,7 @@ import 'localdatabase.dart';
 import 'package:drift/drift.dart';
 
 extension LabaDao on AppDatabase {
+  // KODE LAMAMU - TETAP DIPAKAI
   Future<Map<String, int>> hitungLabaBulanan(DateTime bulan) async {
     final awal = DateTime(bulan.year, bulan.month, 1);
     final akhir = DateTime(bulan.year, bulan.month + 1, 0, 23, 59);
@@ -13,4 +14,15 @@ extension LabaDao on AppDatabase {
     int bebanOps = bebanOpsList.fold(0, (p, e) => p + e.nominal);
     return {'pendapatan': pendapatan, 'bebanGaji': bebanGaji, 'bebanOps': bebanOps, 'labaKotor': pendapatan - bebanGaji, 'labaBersih': pendapatan - bebanGaji - bebanOps};
   }
+
+  // KODE BARU - KALKULATOR SIMULASI PER PERIODE
+  Future<int> hitungBebanGajiPeriode(DateTime mulai, DateTime selesai) async {
+    final data = await (select(gajiMingguan)..where((t) => t.mingguMulai.isBiggerOrEqualValue(mulai) & t.mingguSelesai.isSmallerOrEqualValue(selesai))).get();
+    return data.fold<int>(0, (sum, e) => sum + e.totalGaji);
+  }
+
+  Future<int> simpanSimulasi(SimulasiLabaCompanion data) => into(simulasiLaba).insert(data);
+  Future<void> updateSimulasi(int id, SimulasiLabaCompanion data) => (update(simulasiLaba)..where((t) => t.id.equals(id))).write(data);
+  Future<void> hapusSimulasi(int id) => (delete(simulasiLaba)..where((t) => t.id.equals(id))).go();
+  Stream<List<SimulasiLabaData>> watchSimulasi() => (select(simulasiLaba)..orderBy([(t) => OrderingTerm.desc(t.tanggalSimulasi)])).watch();
 }
