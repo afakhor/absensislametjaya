b2ce1c585bfe3f0cdb56d50a2cc99bc6c08b1581
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-22 11:45:14 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'navs/absen_page.dart';
 import 'navs/gaji_page.dart';
 import 'navs/laba_page.dart';
@@ -11,6 +12,24 @@ class MainMenu extends StatefulWidget { const MainMenu({super.key}); @override S
 class _MainMenuState extends State<MainMenu> {
   int _idx = 0;
   final pages = [const AbsenPage(), const GajiPage(), const LabaPage(), const AuditPage(), const SettingOwnerPage()];
+
+  @override
+  void initState() {
+    super.initState();
+    _requestAllPermissionsAwal();
+  }
+
+  Future<void> _requestAllPermissionsAwal() async {
+    await [
+      Permission.camera,
+      Permission.photos,
+      Permission.storage,
+      Permission.location,
+      Permission.bluetoothScan,
+      Permission.bluetoothConnect,
+    ].request();
+  }
+
   @override Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("TB. SLAMET JAYA", style: TextStyle(fontWeight: FontWeight.bold)), backgroundColor: Colors.orange.shade800, foregroundColor: Colors.white),
