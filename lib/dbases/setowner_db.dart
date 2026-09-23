c949f88daf7633792912c8ed2be0bf7833ac266b
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-24 00:29:28 +0700

    Update setowner_db.dart

--- lib/dbases/setowner_db.dart
+++ lib/dbases/setowner_db.dart
@@ -5,14 +5,11 @@ extension SetOwnerDao on AppDatabase {
   Stream<List<KategoriKaryawanData>> watchKategori() => select(kategoriKaryawan).watch();
   Stream<List<KaryawanData>> watchKaryawan() => select(karyawan).watch();
 
-  Future<int> tambahKategori(String nama, int tarifPerHari) =>
-      into(kategoriKaryawan).insert(KategoriKaryawanCompanion.insert(namaKategori: nama, tarifPerHari: tarifPerHari));
-
-  Future<void> updateTarifKategori(int id, int tarifPerHari) =>
-      (update(kategoriKaryawan)..where((t) => t.id.equals(id))).write(KategoriKaryawanCompanion(tarifPerHari: Value(tarifPerHari)));
-
+  Future<int> tambahKategori(String nama, int tarif) =>
+      into(kategoriKaryawan).insert(KategoriKaryawanCompanion.insert(namaKategori: nama, tarifPerHari: tarif));
+  Future<void> updateTarifKategori(int id, int tarif) =>
+      (update(kategoriKaryawan)..where((t) => t.id.equals(id))).write(KategoriKaryawanCompanion(tarifPerHari: Value(tarif)));
   Future<int> tambahKaryawan(String nama, int kategoriId, String? fotoPath) =>
       into(karyawan).insert(KaryawanCompanion.insert(nama: nama, kategoriId: kategoriId, fotoPath: Value(fotoPath)));
-
   Future<void> hapusKaryawan(int id) => (delete(karyawan)..where((t) => t.id.equals(id))).go();
 }
