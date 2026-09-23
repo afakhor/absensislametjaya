38da4a667958b1a9bd662759403ae8e99096a42e
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-23 11:16:09 +0700

    Update setowner_db.dart

--- lib/dbases/setowner_db.dart
+++ lib/dbases/setowner_db.dart
@@ -2,18 +2,14 @@ import 'package:drift/drift.dart';
 import 'localdatabase.dart';
 
 extension SetOwnerDao on AppDatabase {
-  // KATEGORI
   Stream<List<KategoriKaryawanData>> watchKategori() => select(kategoriKaryawan).watch();
-
-  // FIX: KARYAWAN - DITAMBAHIN DISINI BIAR setting_owner.dart & dashboard_page.dart KEBACA
   Stream<List<KaryawanData>> watchKaryawan() => select(karyawan).watch();
-  Stream<List<KaryawanData>> watchKaryawanAbsen() => select(karyawan).watch();
 
   Future<int> tambahKategori(String nama, int tarifPerHari) =>
       into(kategoriKaryawan).insert(KategoriKaryawanCompanion.insert(namaKategori: nama, tarifPerHari: tarifPerHari));
 
   Future<void> updateTarifKategori(int id, int tarifPerHari) =>
-      (update(kategoriKaryawan)..where((t) => t.id.equals(id))).write(KategoriKaryawanCompanion(tarifPerHari: Value(tarifPerHari)));
+      (update(kategoriKaryawan)..where((t) => t.id.equals(id))).write(KategoriKaryawanCompanion(tarifPerHari: Value(tarifPerHari))));
 
   Future<int> tambahKaryawan(String nama, int kategoriId, String? fotoPath) =>
       into(karyawan).insert(KaryawanCompanion.insert(nama: nama, kategoriId: kategoriId, fotoPath: Value(fotoPath)));
