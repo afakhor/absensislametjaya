d5dbbe990796a3677d860ad12d52a08f0a889b4d
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-24 19:16:31 +0700

    Update dashboard_page.dart

--- lib/navs/dashboard_page.dart
+++ lib/navs/dashboard_page.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:drift/drift.dart';
+import 'package:drift/drift.dart' hide Column;
 import '../dbases/localdatabase.dart';
 import '../dbases/setowner_db.dart';
 import '../dbases/absen_db.dart';
