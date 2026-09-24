e6c664529ab32b3f335864bfe7283f62b4d02cc0
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-24 09:21:47 +0700

    Update laba_db.dart

--- lib/dbases/laba_db.dart
+++ lib/dbases/laba_db.dart
@@ -2,22 +2,32 @@ import 'localdatabase.dart';
 import 'package:drift/drift.dart';
 
 extension LabaDao on AppDatabase {
-  // KODE LAMAMU - TETAP DIPAKAI
   Future<Map<String, int>> hitungLabaBulanan(DateTime bulan) async {
     final awal = DateTime(bulan.year, bulan.month, 1);
     final akhir = DateTime(bulan.year, bulan.month + 1, 0, 23, 59);
-    final gajiBulan = await (select(gajiMingguan)..where((t) => t.mingguMulai.isBiggerOrEqualValue(awal) & t.mingguMulai.isSmallerOrEqualValue(akhir))).get();
-    int bebanGaji = gajiBulan.fold(0, (p, e) => p + e.totalGaji);
-    final pendapatanList = await (select(transaksi)..where((t) => t.jenis.equals('PENDAPATAN') & t.tanggal.isBetweenValues(awal, akhir))).get();
-    int pendapatan = pendapatanList.fold(0, (p, e) => p + e.nominal);
-    final bebanOpsList = await (select(transaksi)..where((t) => t.jenis.equals('BEBAN_OPERASIONAL') & t.tanggal.isBetweenValues(awal, akhir))).get();
-    int bebanOps = bebanOpsList.fold(0, (p, e) => p + e.nominal);
+    final gajiBulan = await (select(gajiMingguan)
+      ..where((t) => t.mingguMulai.isBiggerOrEqualValue(awal))
+      ..where((t) => t.mingguMulai.isSmallerOrEqualValue(akhir))
+    ).get();
+    int bebanGaji = gajiBulan.fold<int>(0, (p, e) => p + e.totalGaji);
+    final pendapatanList = await (select(transaksi)
+      ..where((t) => t.jenis.equals('PENDAPATAN'))
+      ..where((t) => t.tanggal.isBetweenValues(awal, akhir))
+    ).get();
+    int pendapatan = pendapatanList.fold<int>(0, (p, e) => p + e.nominal);
+    final bebanOpsList = await (select(transaksi)
+      ..where((t) => t.jenis.equals('BEBAN_OPERASIONAL'))
+      ..where((t) => t.tanggal.isBetweenValues(awal, akhir))
+    ).get();
+    int bebanOps = bebanOpsList.fold<int>(0, (p, e) => p + e.nominal);
     return {'pendapatan': pendapatan, 'bebanGaji': bebanGaji, 'bebanOps': bebanOps, 'labaKotor': pendapatan - bebanGaji, 'labaBersih': pendapatan - bebanGaji - bebanOps};
   }
 
-  // KODE BARU - KALKULATOR SIMULASI PER PERIODE
   Future<int> hitungBebanGajiPeriode(DateTime mulai, DateTime selesai) async {
-    final data = await (select(gajiMingguan)..where((t) => t.mingguMulai.isBiggerOrEqualValue(mulai) & t.mingguSelesai.isSmallerOrEqualValue(selesai))).get();
+    final data = await (select(gajiMingguan)
+      ..where((t) => t.mingguMulai.isBiggerOrEqualValue(mulai))
+      ..where((t) => t.mingguSelesai.isSmallerOrEqualValue(selesai))
+    ).get();
     return data.fold<int>(0, (sum, e) => sum + e.totalGaji);
   }
 
