416e3a7deb784d1976917fba0080a589390fd527
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-25 20:53:51 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'navs/absen_page.dart';
 import 'navs/gaji_page.dart';
 import 'navs/dashboard_page.dart';
 import 'navs/laba_page.dart';
-import 'navs/settingowner.dart';
+import 'navs/setting_owner.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
