fec2439e8d239d3904372e97cd72641fab442cd4
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-25 20:37:43 +0700

    Update laba_db.dart

--- lib/dbases/laba_db.dart
+++ lib/dbases/laba_db.dart
@@ -1,38 +1,61 @@
-import 'localdatabase.dart';
 import 'package:drift/drift.dart';
+import 'localdatabase.dart';
 
 extension LabaDao on AppDatabase {
   Future<Map<String, int>> hitungLabaBulanan(DateTime bulan) async {
-    final awal = DateTime(bulan.year, bulan.month, 1);
-    final akhir = DateTime(bulan.year, bulan.month + 1, 0, 23, 59);
+    final awal = DateTime(bulan.year, bulan.month, 1, 0, 0, 0);
+    final akhir = DateTime(bulan.year, bulan.month + 1, 0, 23, 59, 59, 999);
+
     final gajiBulan = await (select(gajiMingguan)
-      ..where((t) => t.mingguMulai.isBiggerOrEqualValue(awal))
-      ..where((t) => t.mingguMulai.isSmallerOrEqualValue(akhir))
-    ).get();
+          ..where((t) => t.mingguMulai.isBiggerOrEqualValue(awal))
+          ..where((t) => t.mingguMulai.isSmallerOrEqualValue(akhir)))
+        .get();
+
     int bebanGaji = gajiBulan.fold<int>(0, (p, e) => p + e.totalGaji);
+
     final pendapatanList = await (select(transaksi)
-      ..where((t) => t.jenis.equals('PENDAPATAN'))
-      ..where((t) => t.tanggal.isBetweenValues(awal, akhir))
-    ).get();
+          ..where((t) => t.jenis.equals('PENDAPATAN'))
+          ..where((t) => t.tanggal.isBetweenValues(awal, akhir)))
+        .get();
+
     int pendapatan = pendapatanList.fold<int>(0, (p, e) => p + e.nominal);
+
     final bebanOpsList = await (select(transaksi)
-      ..where((t) => t.jenis.equals('BEBAN_OPERASIONAL'))
-      ..where((t) => t.tanggal.isBetweenValues(awal, akhir))
-    ).get();
+          ..where((t) => t.jenis.equals('BEBAN_OPERASIONAL'))
+          ..where((t) => t.tanggal.isBetweenValues(awal, akhir)))
+        .get();
+
     int bebanOps = bebanOpsList.fold<int>(0, (p, e) => p + e.nominal);
-    return {'pendapatan': pendapatan, 'bebanGaji': bebanGaji, 'bebanOps': bebanOps, 'labaKotor': pendapatan - bebanGaji, 'labaBersih': pendapatan - bebanGaji - bebanOps};
+
+    return {
+      'pendapatan': pendapatan,
+      'bebanGaji': bebanGaji,
+      'bebanOps': bebanOps,
+      'labaKotor': pendapatan - bebanGaji,
+      'labaBersih': pendapatan - bebanGaji - bebanOps,
+    };
   }
 
   Future<int> hitungBebanGajiPeriode(DateTime mulai, DateTime selesai) async {
+    final start = DateTime(mulai.year, mulai.month, mulai.day, 0, 0, 0);
+    final end = DateTime(selesai.year, selesai.month, selesai.day, 23, 59, 59, 999);
+
     final data = await (select(gajiMingguan)
-      ..where((t) => t.mingguMulai.isBiggerOrEqualValue(mulai))
-      ..where((t) => t.mingguSelesai.isSmallerOrEqualValue(selesai))
-    ).get();
+          ..where((t) => t.mingguMulai.isBiggerOrEqualValue(start))
+          ..where((t) => t.mingguSelesai.isSmallerOrEqualValue(end)))
+        .get();
+
     return data.fold<int>(0, (sum, e) => sum + e.totalGaji);
   }
 
   Future<int> simpanSimulasi(SimulasiLabaCompanion data) => into(simulasiLaba).insert(data);
-  Future<void> updateSimulasi(int id, SimulasiLabaCompanion data) => (update(simulasiLaba)..where((t) => t.id.equals(id))).write(data);
-  Future<void> hapusSimulasi(int id) => (delete(simulasiLaba)..where((t) => t.id.equals(id))).go();
-  Stream<List<SimulasiLabaData>> watchSimulasi() => (select(simulasiLaba)..orderBy([(t) => OrderingTerm.desc(t.tanggalSimulasi)])).watch();
+
+  Future<void> updateSimulasi(int id, SimulasiLabaCompanion data) =>
+      (update(simulasiLaba)..where((t) => t.id.equals(id))).write(data);
+
+  Future<void> hapusSimulasi(int id) =>
+      (delete(simulasiLaba)..where((t) => t.id.equals(id))).go();
+
+  Stream<List<SimulasiLabaData>> watchSimulasi() =>
+      (select(simulasiLaba)..orderBy([(t) => OrderingTerm.desc(t.tanggalSimulasi)])).watch();
 }
