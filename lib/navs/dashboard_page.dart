4879a88022d271cfd31c362d1ffb0e2b8a3e0235
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-25 20:46:53 +0700

    Update dashboard_page.dart

--- lib/navs/dashboard_page.dart
+++ lib/navs/dashboard_page.dart
@@ -281,7 +281,7 @@ class _DashboardPageState extends State<DashboardPage> {
                               children: [
                                 const Text(
                                   "📝 LOG OMSET & OPERASIONAL HARIAN",
-                                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue.shade900),
+                                  style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFF0D47A1)),
                                 ),
                                 const SizedBox(height: 8),
                                 TextField(
