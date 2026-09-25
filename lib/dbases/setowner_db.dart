11848f970a8da05089cd305324f064244d7115b7
afakhor <31812946+afakhor@users.noreply.github.com>
2026-09-25 17:39:31 +0700

    Update setowner_db.dart

--- lib/dbases/setowner_db.dart
+++ lib/dbases/setowner_db.dart
@@ -7,9 +7,12 @@ extension SetOwnerDao on AppDatabase {
 
   Future<int> tambahKategori(String nama, int tarif) =>
       into(kategoriKaryawan).insert(KategoriKaryawanCompanion.insert(namaKategori: nama, tarifPerHari: tarif));
+      
   Future<void> updateTarifKategori(int id, int tarif) =>
       (update(kategoriKaryawan)..where((t) => t.id.equals(id))).write(KategoriKaryawanCompanion(tarifPerHari: Value(tarif)));
+      
   Future<int> tambahKaryawan(String nama, int kategoriId, String? fotoPath) =>
       into(karyawan).insert(KaryawanCompanion.insert(nama: nama, kategoriId: kategoriId, fotoPath: Value(fotoPath)));
+      
   Future<void> hapusKaryawan(int id) => (delete(karyawan)..where((t) => t.id.equals(id))).go();
 }
